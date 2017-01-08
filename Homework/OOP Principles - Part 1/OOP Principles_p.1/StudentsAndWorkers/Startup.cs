e3f092b13afe2fcef0f31120a52ad54ffc5fb7a9
Mihaela_Iv <mihaela.ivanova.ivanova@gmail.com>
2017-01-09 00:12:33 +0200

    Srudents and workers task done

--- Homework/OOP Principles - Part 1/OOP Principles_p.1/StudentsAndWorkers/Startup.cs
+++ Homework/OOP Principles - Part 1/OOP Principles_p.1/StudentsAndWorkers/Startup.cs
@@ -31,6 +31,36 @@ namespace StudentsAndWorkers
                 Console.WriteLine($"{student.FullName} - Grade:{student.Grade}");
             }
 
+            List<Worker> workers = new List<Worker>
+            {
+                new Worker("Stoyan", "Asenov",840,6),
+                new Worker("Venci", "Argirov",220,5),
+                new Worker("Asya", "Lilova",610,7),
+                new Worker("Tanya", "Ivanova",470,8),
+                new Worker("Kristina", "Kamenova",280,8),
+                new Worker("Veselin", "Ivanov",190,6),
+                new Worker("Krasimir", "Stoyanov",140,7),
+                new Worker("Lili", "Zaharieva",1000,7),
+                new Worker("Tihomir", "Asenov",1100,8),
+                new Worker("Alexandra", "Biyazova",130,8)
+            };
+
+            var orderedWorkers = workers.OrderBy(worker => worker.MoneyPerHour());
+            foreach (var worker in orderedWorkers)
+            {
+                Console.WriteLine($"{worker.FullName} - Salary per hour:{worker.MoneyPerHour():F2}");
+            }
+
+            List<Human> mergedList =new List<Human>();
+            mergedList.AddRange(students);
+            mergedList.AddRange(workers);
+
+            var sortedPeople = mergedList.OrderBy(person => person.FirstName)
+                .ThenBy(person => person.LastName);
+            foreach(var person in sortedPeople)
+            {
+                Console.WriteLine(person.FullName);
+            }
         }
     }
 }
